c9fb5bfa81507047672f3b3593f1687e36589063
MEIYAZHAGANgit <meiyazhaganmailbox@gmail.com>
2024-03-19 12:00:37 +0530

    first commit

---
+++ src/pages/Home/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="py-10">
+   <div className=" h-[1px] w-full bg-gray-700">
+
+   </div>
+   <div className=" flex items-center justify-center flex-col mt-10">
+    
+    <h className=" text-white">
+     <a href="Resume.pdf" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">RESUME </a>
+    </h>
+   </div>
+
+    </div>
+  )
+}
+
+export default Footer;
